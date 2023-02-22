e118c5d2ace8736eacf10d854630d450865b271f
Azam <azamtanoli00@gmail.com>
2023-02-22 15:07:10 +0500

    change app.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. By Azam Dev
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer">
-          Learn React Running
+          Learn React
         </a>
       </header>
     </div>
